bfb712f5b9a24582a70dd6a5696fb1c27f3e31df
ianbeltran427 <ip.beltran@uniandes.edu.co>
2024-05-05 17:58:13 -0500

    Ajusta llamado de funciones, dado que hay dos de manera igua, no se aplica sobrecarga de la función

--- kraken/features/web/step_definitions/profile_steps.js
+++ kraken/features/web/step_definitions/profile_steps.js
@@ -4,63 +4,63 @@ const ProfilePage = require('../model/profilePage');
 
 let profilePage;
 
-BeforeAll(async function () {    
-    profilePage = new ProfilePage();   
+BeforeAll(async function () {
+    profilePage = new ProfilePage();
 })
 
-When('I click profile', async function() {
-    profilePage.setDriver(this.driver);    
+When('I click profile', async function () {
+    profilePage.setDriver(this.driver);
     profilePage.submitProfileMenu();
 });
 
-When('I click on your profile', async function() {    
+When('I click on your profile', async function () {
     profilePage.submitYourProfile();
 });
 
-When('I click confirm password', async function() {    
-    profilePage.submitChangeButton(/*"Change password"*/);
+When('I click confirm password', async function () {
+    profilePage.submitChangeButtonInsecure();
 });
 
-When('I enter old password {kraken-string}', async function (password) {    
+When('I enter old password {kraken-string}', async function (password) {
     profilePage.typeOldPassword(password);
 });
 
-When('I enter the new password {kraken-string}', async function (password) {    
+When('I enter the new password {kraken-string}', async function (password) {
     profilePage.typePassword(password);
 });
 
-When('I both enter and confirm the new password {kraken-string}', async function (password) {    
+When('I both enter and confirm the new password {kraken-string}', async function (password) {
     profilePage.typeConfirmPassword(password);
 });
 
-Then('I validate the error message password {string}', async function(errorMessage) {
-    profilePage.validateErrorMessage(errorMessage);   
+Then('I validate the error message password {string}', async function (errorMessage) {
+    profilePage.validateErrorMessage(errorMessage);
 });
 
-Then('I validate the error message old password alert {string}', async function(errorMessage) {
-    profilePage.validateErrorMessageAlert(errorMessage);   
+Then('I validate the error message old password alert {string}', async function (errorMessage) {
+    profilePage.validateErrorMessageAlert(errorMessage);
 });
 
-Then('I click close and save', async function() {   
+Then('I click close and save', async function () {
     profilePage.submitChangeButton("Save & close");
 });
 
-Then('I click sign out', async function() {   
+Then('I click sign out', async function () {
     profilePage.submitSignOut();
 });
 
-When('I change FullName {string}', async function(title) {
+When('I change FullName {string}', async function (title) {
     profilePage.setFullName(title);
 });
 
-When('I click Save button', async function() {
+When('I click Save button', async function () {
     profilePage.submitChangeButton();
 });
 
-When('I click back on profile', async function() {
+When('I click back on profile', async function () {
     profilePage.submitProfileMenu();
 });
 
-Then('I validate the name has been changed {string}', async function(name) {
+Then('I validate the name has been changed {string}', async function (name) {
     profilePage.validateProfileName(name);
 });
