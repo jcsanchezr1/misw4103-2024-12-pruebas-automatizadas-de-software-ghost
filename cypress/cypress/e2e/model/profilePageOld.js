2db059e3184f10946b3e79f57435c4fa2080d476
jcsanchezr1 <jc.sanchezr1@uniandes.edu.co>
2024-05-07 22:08:21 -0500

    Se genera primera version de los escenarios de profile para la version 3.4.2. de Ghost

---
+++ cypress/cypress/e2e/model/profilePageOld.js
@@ -0,0 +1,60 @@
+class ProfilePageOld {
+      
+    constructor() {
+        this.elements = {
+            profileMenu: '.gh-user-avatar',
+            buttonYourProfile: 'ul li a[href="#/settings/staff/conan/"]',
+            buttonSignOut: 'a.user-menu-signout',
+            buttonChangePassword: '.gh-btn.gh-btn-icon.button-change-password.gh-btn-red',
+            buttonSaveProfile: '.gh-btn.gh-btn-primary.gh-btn-icon',
+            inputOldPassword: '#user-password-old',
+            inputPassword: '#user-password-new',
+            inputConfirmPassword: '#user-new-password-verification',
+            labeInputPassword: '.form-group.error p',
+            alertInputPassword: '.gh-alerts article.gh-alert div.gh-alert-content',
+            fullName: '#user-name',
+            save:'body > div.gh-app > div > main > section > div > header > section > button',
+            saved: 'gh-btn gh-btn-primary gh-btn-icon gh-btn-green ember-view',
+            profileLabel:'.gh-user-name'
+        };
+    }
+
+    submitProfileMenu() {
+        cy.get(this.elements.profileMenu).click();
+    }
+
+    typeName(name) {
+        cy.get(this.elements.fullName).clear();
+        cy.get(this.elements.fullName).type(name, {force: true});
+    }
+
+    submitYourProfile() {
+        cy.get(this.elements.buttonYourProfile).click();
+    }
+
+    submitChangePassword() {
+        cy.get(this.elements.buttonChangePassword).click();
+    }   
+
+    submitSignOut() {
+        cy.get(this.elements.buttonSignOut).click();
+    }     
+
+    submitSaveButton() {
+        cy.get(this.elements.buttonSaveProfile).click();
+    }
+
+    typeOldPassword(password) {
+        cy.get(this.elements.inputOldPassword).type(password, {force: true});     
+    }
+
+    typePassword(password) {
+        cy.get(this.elements.inputPassword).type(password, {force: true});
+    }
+
+    typeConfirmPassword(password) {
+        cy.get(this.elements.inputConfirmPassword).type(password, {force: true});
+    }    
+}
+
+module.exports = ProfilePageOld;
---
+++ cypress/cypress/e2e/profileOld.cy.js
@@ -0,0 +1,133 @@
+import properties from './properties.json';
+import LoginPageOld from './model/loginPageOld';
+import ProfilePageOld from './model/profilePageOld';
+
+const profilePageOld = new ProfilePageOld();
+const loginPageOld = new LoginPageOld();
+
+describe('Funcionalidad de Members', () => {
+    beforeEach(() => {
+        // Given I navigate to page "<URL>"
+        loginPageOld.VisitURL(properties.URL_OLD);
+        cy.wait(1000);
+        // AND I enter email "<EMAIL>"
+        loginPageOld.typeEmail(properties.EMAIL);
+        // AND I enter password "<PASSWORD>"
+        loginPageOld.typePassword(properties.PASSWORD);
+        // AND I click sign in
+        loginPageOld.clickSignInButton();
+        cy.wait(3000);
+    });
+
+    it('Cambiar nombre de manera éxitosa', () => {
+        // WHEN I click profile
+        profilePageOld.submitProfileMenu();
+        cy.wait(1000);
+        // AND I click on your profile
+        profilePageOld.submitYourProfile()
+        cy.wait(1000);
+        // AND I enter name "NombreNuevo"
+        profilePageOld.typeName('NombreNuevo');
+        cy.wait(1000);
+        // AND I click save
+        profilePageOld.submitSaveButton();
+        cy.wait(1000);
+        // AND I click profile
+        profilePageOld.submitProfileMenu();
+        cy.wait(1000);
+        // AND I validate the name "NombreNuevo"
+        cy.get(profilePageOld.elements.profileLabel).should('contain', 'NombreNuevo');
+    })
+
+    it('Cambiar password de forma fallida por error insecure', () => {
+        // WHEN I click profile
+        profilePageOld.submitProfileMenu();
+        cy.wait(1000);
+        // AND I click on your profile
+        profilePageOld.submitYourProfile()
+        cy.wait(1000);
+        // AND I enter password "<PASSWORD>"
+        profilePageOld.typeOldPassword(properties.PASSWORD);
+        cy.wait(1000);
+        // AND I enter the new password "password1234"
+        profilePageOld.typePassword('password1234');
+        cy.wait(1000);
+        // AND I both enter and confirm the new password "password1234"
+        profilePageOld.typeConfirmPassword("password1234");
+        cy.wait(1000);
+        // AND I click confirm password
+        profilePageOld.submitChangePassword();
+        cy.wait(1000);
+        // THEN I validate the error message old password alert "Sorry, you cannot use an insecure password."
+        cy.get(profilePageOld.elements.labeInputPassword).should('contain', 'Sorry, you cannot use an insecure password.');
+        // AND I click save
+        profilePageOld.submitSaveButton();
+        cy.wait(1000);
+        // AND I click profile
+        profilePageOld.submitProfileMenu();
+        cy.wait(1000);
+        // AND I click sign out
+        profilePageOld.submitSignOut();
+        cy.wait(1000);
+        // AND I enter email "<EMAIL>"
+        loginPageOld.typeEmail(properties.EMAIL);
+        // AND I enter password "password1234"
+        loginPageOld.typePassword('password1234');
+        // AND I click sign in
+        loginPageOld.clickSignInButton();
+        cy.wait(3000);
+        // AND I clean password
+        loginPageOld.cleanPassword();
+        // AND I enter password "<PASSWORD>"
+        loginPageOld.typePassword(properties.PASSWORD);
+        // AND I click sign in
+        loginPageOld.clickSignInButton();
+        cy.wait(3000);
+    });
+
+    it('Cambiar password de forma fallida por error de longitud de la contraseña', () => {
+        // WHEN I click profile
+        profilePageOld.submitProfileMenu();
+        cy.wait(1000);
+        // AND I click on your profile
+        profilePageOld.submitYourProfile()
+        cy.wait(1000);
+        // AND I enter password "<PASSWORD>"
+        profilePageOld.typeOldPassword(properties.PASSWORD);
+        cy.wait(1000);
+        // AND I enter the new password "123456789"
+        profilePageOld.typePassword('123456789');
+        cy.wait(1000);
+        // AND I both enter and confirm the new password "123456789"
+        profilePageOld.typeConfirmPassword("123456789");
+        cy.wait(1000);
+        // AND I click confirm password
+        profilePageOld.submitChangePassword();
+        cy.wait(1000);
+        // THEN I validate the error message alert "Password must be at least 10 characters long."
+        cy.get(profilePageOld.elements.labeInputPassword).should('contain', 'Password must be at least 10 characters long.');
+        // AND I click save
+        profilePageOld.submitSaveButton();
+        cy.wait(1000);
+        // AND I click profile
+        profilePageOld.submitProfileMenu();
+        cy.wait(1000);
+        // AND I click sign out
+        profilePageOld.submitSignOut();
+        cy.wait(1000);
+        // AND I enter email "<EMAIL>"
+        loginPageOld.typeEmail(properties.EMAIL);
+        // AND I enter password "123456789"
+        loginPageOld.typePassword('123456789');
+        // AND I click sign in
+        loginPageOld.clickSignInButton();
+        cy.wait(3000);
+        // AND I clean password
+        loginPageOld.cleanPassword();
+        // AND I enter password "<PASSWORD>"
+        loginPageOld.typePassword(properties.PASSWORD);
+        // AND I click sign in
+        loginPageOld.clickSignInButton();
+        cy.wait(3000);
+    });
+});
