69da509f3df089ba7cab341759ba9f45efed8d26
jcsanchezr1 <jc.sanchezr1@uniandes.edu.co>
2024-05-17 01:02:02 -0500

    Se anade escenarios a priori profile en cypress

---
+++ cypress/cypress/e2e/profile_aleatorio.cy.js
@@ -0,0 +1,196 @@
+import properties from './properties.json';
+import LoginPage from './model/loginPage';
+import ProfilePage from './model/profilePage';
+import CommonFunction from "./model/commonFunction";
+
+const profilePage = new ProfilePage();
+const loginPage = new LoginPage();
+const commonFunction = new CommonFunction();
+
+let parentFolder = '';
+
+
+describe("Funcionalidad de Profile Aleatorio", (z ) => {
+    beforeEach(() => {
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            return false;
+        });
+        Cypress.Screenshot.defaults({
+            overwrite: true,
+        })
+    });
+
+    it('Cambiar nombre de manera éxitosa', () => {
+        parentFolder = Cypress.mocha.getRunner().suite.ctx._runnable.parent.title + '/' + Cypress.mocha.getRunner().suite.ctx._runnable.title + '/';
+        commonFunction.setPath(parentFolder)
+        //GIVEN un usuario admin logueado en Ghost
+        commonFunction.login(properties.URL)
+        // WHEN I click profile
+        profilePage.submitProfileMenu();
+        commonFunction.wait(1000);
+        // AND I click on your profile
+        profilePage.submitYourProfile()
+        commonFunction.wait(1000);
+        // AND I enter name "NombreNuevo"
+        profilePage.typeName('NombreNuevo');
+        commonFunction.wait(1000);
+        // AND I click save
+        profilePage.submitSaveButton();
+        commonFunction.wait(1000);
+        // AND I click profile
+        profilePage.submitProfileMenu();
+        commonFunction.wait(1000);
+        // AND I validate the name "NombreNuevo"
+        cy.get(profilePage.elements.profileLabel).should('contain', 'NombreNuevo');
+    })
+
+    it('Cambiar password de forma fallida por error insecure', () => {
+        parentFolder = Cypress.mocha.getRunner().suite.ctx._runnable.parent.title + '/' + Cypress.mocha.getRunner().suite.ctx._runnable.title + '/';
+        commonFunction.setPath(parentFolder)
+        //GIVEN un usuario admin logueado en Ghost
+        commonFunction.login(properties.URL)
+        // WHEN I click profile
+        profilePage.submitProfileMenu();
+        commonFunction.wait(1000);
+        // AND I click on your profile
+        profilePage.submitYourProfile()
+        commonFunction.wait(1000);
+        // AND I enter password "<PASSWORD>"
+        profilePage.typeOldPassword(properties.PASSWORD);
+        commonFunction.wait(1000);
+        // AND I enter the new password "password1234"
+        profilePage.typePassword('password1234');
+        commonFunction.wait(1000);
+        // AND I both enter and confirm the new password "password1234"
+        profilePage.typeConfirmPassword("password1234");
+        commonFunction.wait(1000);
+        // AND I click confirm password
+        profilePage.submitChangePassword();
+        commonFunction.wait(1000);
+        // THEN I validate the error message old password alert "Sorry, you cannot use an insecure password."
+        cy.get(profilePage.elements.labeInputPassword).should('contain', 'Sorry, you cannot use an insecure password.');
+        // AND I click save
+        profilePage.submitSaveButton();
+        commonFunction.wait(1000);
+        // AND I click profile
+        profilePage.submitProfileMenu();
+        commonFunction.wait(1000);
+        // AND I click sign out
+        profilePage.submitSignOut();
+        commonFunction.wait(1000);
+        // AND I enter email "<EMAIL>"
+        loginPage.typeEmail(properties.EMAIL);
+        // AND I enter password "password1234"
+        loginPage.typePassword('password1234');
+        // AND I click sign in
+        loginPage.clickSignInButton();
+        commonFunction.wait(3000);
+        // AND I clean password
+        loginPage.cleanPassword();
+        // AND I enter password "<PASSWORD>"
+        loginPage.typePassword(properties.PASSWORD);
+        // AND I click sign in
+        loginPage.clickSignInButton();
+        commonFunction.wait(3000);
+    });
+
+    it('Cambiar password de forma fallida por error de longitud de la contraseña', () => {
+        parentFolder = Cypress.mocha.getRunner().suite.ctx._runnable.parent.title + '/' + Cypress.mocha.getRunner().suite.ctx._runnable.title + '/';
+        commonFunction.setPath(parentFolder)
+        //GIVEN un usuario admin logueado en Ghost
+        commonFunction.login(properties.URL)
+        // WHEN I click profile
+        profilePage.submitProfileMenu();
+        commonFunction.wait(1000);
+        // AND I click on your profile
+        profilePage.submitYourProfile()
+        commonFunction.wait(1000);
+        // AND I enter password "<PASSWORD>"
+        profilePage.typeOldPassword(properties.PASSWORD);
+        commonFunction.wait(1000);
+        // AND I enter the new password "123456789"
+        profilePage.typePassword('123456789');
+        commonFunction.wait(1000);
+        // AND I both enter and confirm the new password "123456789"
+        profilePage.typeConfirmPassword("123456789");
+        commonFunction.wait(1000);
+        // AND I click confirm password
+        profilePage.submitChangePassword();
+        commonFunction.wait(1000);
+        // THEN I validate the error message alert "Password must be at least 10 characters long."
+        cy.get(profilePage.elements.labeInputPassword).should('contain', 'Password must be at least 10 characters long.');
+        // AND I click save
+        profilePage.submitSaveButton();
+        commonFunction.wait(1000);
+        // AND I click profile
+        profilePage.submitProfileMenu();
+        commonFunction.wait(1000);
+        // AND I click sign out
+        profilePage.submitSignOut();
+        commonFunction.wait(1000);
+        // AND I enter email "<EMAIL>"
+        loginPage.typeEmail(properties.EMAIL);
+        // AND I enter password "123456789"
+        loginPage.typePassword('123456789');
+        // AND I click sign in
+        loginPage.clickSignInButton();
+        commonFunction.wait(3000);
+        // AND I clean password
+        loginPage.cleanPassword();
+        // AND I enter password "<PASSWORD>"
+        loginPage.typePassword(properties.PASSWORD);
+        // AND I click sign in
+        loginPage.clickSignInButton();
+        commonFunction.wait(3000);
+    });
+
+    it('Cambiar password de forma fallida por error old password incorrecto', () => {
+        parentFolder = Cypress.mocha.getRunner().suite.ctx._runnable.parent.title + '/' + Cypress.mocha.getRunner().suite.ctx._runnable.title + '/';
+        commonFunction.setPath(parentFolder)
+        //GIVEN un usuario admin logueado en Ghost
+        commonFunction.login(properties.URL)
+        // WHEN I click profile
+        profilePage.submitProfileMenu();
+        commonFunction.wait(1000);
+        // AND I click on your profile
+        profilePage.submitYourProfile()
+        commonFunction.wait(1000);
+        // AND I enter old password "OldPswInvalido"
+        profilePage.typeOldPassword('OldPswInvalido');
+        commonFunction.wait(1000);
+        // AND I enter the new password "EstoEsUnPswValido!"
+        profilePage.typePassword('EstoEsUnPswValido!');
+        commonFunction.wait(1000);
+        // AND I both enter and confirm the new password "EstoEsUnPswValido!"
+        profilePage.typeConfirmPassword("EstoEsUnPswValido!");
+        commonFunction.wait(1000);
+        // AND I click confirm password
+        profilePage.submitChangePassword();
+        commonFunction.wait(1000);
+        // THEN I validate the error message old password alert "Your password is incorrect. Your password is incorrect."
+        cy.get(profilePage.elements.alertInputPassword).should('contain', 'Your password is incorrect. Your password is incorrect.');
+        // AND I click save
+        profilePage.submitSaveButton();
+        commonFunction.wait(1000);
+        // AND I click profile
+        profilePage.submitProfileMenu();
+        commonFunction.wait(1000);
+        // AND I click sign out
+        profilePage.submitSignOut();
+        commonFunction.wait(1000);
+        // AND I enter email "<EMAIL>"
+        loginPage.typeEmail(properties.EMAIL);
+        // AND I enter password "EstoEsUnPswValido!"
+        loginPage.typePassword('EstoEsUnPswValido!');
+        // AND I click sign in
+        loginPage.clickSignInButton();
+        commonFunction.wait(3000);
+        // AND I clean password
+        loginPage.cleanPassword();
+        // AND I enter password "<PASSWORD>"
+        loginPage.typePassword(properties.PASSWORD);
+        // AND I click sign in
+        loginPage.clickSignInButton();
+        commonFunction.wait(3000);
+    });
+});
